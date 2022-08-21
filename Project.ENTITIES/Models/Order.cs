019fe003335a2483bb7ce3acfbf3725751c1a313
onur <onur@LAPTOP-KKPTL8JD>
2022-08-21 22:02:37 +0300

    Add project files.

---
+++ Project.ENTITIES/Models/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public class Order:BaseEntity
+    {
+        public string ShippedAddress { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        //Sipariş işlemlerinin icerisindeki bilgileri daha rahat yakalamak adına actıgımız property'lerdern bir tanesi de TotalPrice'dir... Yalnız burada cok dikkatli olmamız gerekir gerçekten size hız kazandıracak bir durum varsa bu bilgileri ek olarak buraya almamız gerekir...Aynı zamanda bu durum abartılmamalıdır... Yani ilgili yapının tüm verileri asla bu sınıfa komple koyulmamalıdır...Sadece spesifik parcalar alınmalıdır
+
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+        public int? AppUserID { get; set; }
+        public int? ShipperID { get; set; }
+
+
+
+        //Relational Properties
+        public virtual AppUser AppUser { get; set; }
+        public virtual List<OrderDetail> OrderDetails { get; set; }
+        public virtual Shipper Shipper { get; set; }
+
+
+
+
+    }
+}
---
+++ Project.MVCUI/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Project.BLL.DesignPatterns.GenericRepository.ConcRep;
+using Project.COMMON.Tools;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.MVCUI.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        AppUserRepository _apRep;
+
+        public HomeController()
+        {
+            _apRep = new AppUserRepository();
+        }
+
+        // GET: Home
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AppUser appUser)
+        {
+            AppUser yakalanan = _apRep.FirstOrDefault(x => x.UserName == appUser.UserName);
+            if(yakalanan == null)
+            {
+                ViewBag.Kullanici = "Kullanici bulunamadı";
+                return View();
+            }
+
+            string decrypted = DantexCrypt.DeCrypt(yakalanan.Password);
+            if(appUser.Password == decrypted && yakalanan.Role == ENTITIES.Enums.UserRole.Admin)
+            {
+                if (!yakalanan.Active) return AktifKontrol();
+                Session["admin"] = yakalanan;
+                return RedirectToAction("CategoryList","Category",new { area="Admin"});
+            }
+            else if (yakalanan.Role == ENTITIES.Enums.UserRole.Member && appUser.Password == decrypted)
+            {
+                if (!yakalanan.Active) return AktifKontrol();
+                Session["member"] = yakalanan;
+                return RedirectToAction("ShoppingList", "Shopping");
+                
+            }
+            ViewBag.Kullanici = "Kullanıcı bulunamadı";
+            return View();
+
+
+        }
+
+        private ActionResult AktifKontrol()
+        {
+            ViewBag.Kullanici = "Lutfen hesabınızı aktif hale getiriniz";
+            return View("Login");
+        }
+    }
+}
